b746be2979de5fe4bb34c8b33edb14d82758805f
Cyrille Quemin <cyrille.quemin@shopify.com>
2022-11-30 17:11:59 -0500

    Add tests on second database on same host

--- packages/shopify-app-session-storage-mysql/src/__tests__/mysql.test.ts
+++ packages/shopify-app-session-storage-mysql/src/__tests__/mysql.test.ts
@@ -11,14 +11,19 @@ import {MySQLSessionStorage} from '../mysql';
 
 const exec = promisify(child_process.exec);
 
+const rootURL = new URL('mysql://root:passify@localhost');
 const dbURL = new URL('mysql://shopify:passify@localhost/shopitest');
+const dbURL2 = new URL('mysql://shopify:passify@localhost/shopitest2');
+
 
 describe('MySQLSessionStorage', () => {
   let storage: MySQLSessionStorage;
+  let storage2: MySQLSessionStorage;
+
   let containerId: string;
   beforeAll(async () => {
     const runCommand = await exec(
-      'podman run -d -e MYSQL_DATABASE=shopitest -e MYSQL_USER=shopify -e MYSQL_PASSWORD=passify -e MYSQL_RANDOM_ROOT_PASSWORD=1 -p 3306:3306 mysql:8-oracle',
+      'podman run -d -e MYSQL_DATABASE=shopitest -e MYSQL_USER=shopify -e MYSQL_PASSWORD=passify -e MYSQL_ROOT_PASSWORD=passify -p 3306:3306 mysql:8-oracle',
       {encoding: 'utf8'},
     );
     containerId = runCommand.stdout.trim();
@@ -26,7 +31,13 @@ describe('MySQLSessionStorage', () => {
     await poll(
       async () => {
         try {
-          const connection = await mysql2.createConnection(dbURL.toString());
+          const connection = await mysql2.createConnection(rootURL.toString());
+
+          await connection.execute(`CREATE DATABASE shopitest2;`);
+          await connection.execute(
+            `GRANT ALL ON shopitest2.* TO 'shopify'@'%';`,
+          );
+
           await connection.end();
         } catch {
           return false;
@@ -36,12 +47,16 @@ describe('MySQLSessionStorage', () => {
       {interval: 500, timeout: 20000},
     );
     storage = new MySQLSessionStorage(dbURL);
+    storage2 = new MySQLSessionStorage(dbURL2);
   });
 
   afterAll(async () => {
     await storage.disconnect();
+    await storage2.disconnect();
+
     await exec(`podman rm -f ${containerId}`);
   });
 
   batteryOfTests(async () => storage);
+  batteryOfTests(async () => storage2);
 });
