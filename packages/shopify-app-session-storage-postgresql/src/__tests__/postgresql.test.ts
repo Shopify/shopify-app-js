91d67690a68ece581e99746521b737c024a92830
Cyrille Quemin <cyrille.quemin@gmail.com>
2022-12-01 16:02:48 -0500

    Adding second session storage as parameter to the test
    Co-authored-by: Kevin O'Sullivan <56687600+mkevinosullivan@users.noreply.github.com>

--- packages/shopify-app-session-storage-postgresql/src/__tests__/postgresql.test.ts
+++ packages/shopify-app-session-storage-postgresql/src/__tests__/postgresql.test.ts
@@ -71,6 +71,14 @@ describe('PostgreSQLSessionStorage', () => {
     await exec(`podman rm -f ${containerId}`);
   });
 
-  batteryOfTests(async () => storage);
-  batteryOfTests(async () => storage2);
+  const tests = [
+    {dbName: 'shopitest', sessionStorage: async () => storage},
+    {dbName: 'shopitest2', sessionStorage: async () => storage2},
+  ];
+
+  for (const {dbName, sessionStorage} of tests) {
+    describe(`with ${dbName}`, () => {
+      batteryOfTests(sessionStorage);
+    });
+  }
 });
