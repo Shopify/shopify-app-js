5d6260305a44ad94a05b744626a2121224783ead
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-12-02 09:28:45 -0500

    Fix build issue and add CI build step

--- packages/shopify-app-express/src/auth/types.ts
+++ packages/shopify-app-express/src/auth/types.ts
@@ -1,7 +1,5 @@
-import {Shopify} from '@shopify/shopify-api';
 import {Request, RequestHandler, Response} from 'express';
 
-import {AppConfigInterface} from '../config-types';
 import {ApiAndConfigParams} from '../types';
 
 export interface AuthMiddleware {
