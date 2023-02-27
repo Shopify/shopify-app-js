b1733ce0222faa859b0228a8b4f314d222a0455c
Cyrille Quemin <2048006+cquemin@users.noreply.github.com>
2023-02-27 12:48:56 +0100

    Remove commented linea

--- packages/shopify-app-session-storage-test-utils/src/index.ts
+++ packages/shopify-app-session-storage-test-utils/src/index.ts
@@ -1,6 +1,2 @@
-// import {batteryOfTests, testMigrationList} from './battery-of-tests';
-// import {poll, wait} from './utils';
-
-// export {batteryOfTests, poll, wait, testMigrationList};
 export * from './battery-of-tests';
 export * from './utils';
