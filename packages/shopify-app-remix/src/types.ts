5d0ce7da0f29288400e41b01cfa231466a08cb31
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-06-27 14:38:06 -0400

    Return error enum from login function

--- packages/shopify-app-remix/src/types.ts
+++ packages/shopify-app-remix/src/types.ts
@@ -37,8 +37,14 @@ type RegisterWebhooks = (
   options: RegisterWebhooksOptions,
 ) => Promise<RegisterReturn>;
 
+export enum LoginErrorType {
+  MissingShop = 'MISSING_SHOP',
+  InvalidShop = 'INVALID_SHOP',
+  InvalidProtocol = 'INVALID_PROTOCOL',
+}
+
 export interface LoginError {
-  errors: {shop: string};
+  shop?: LoginErrorType;
 }
 
 type Login = (request: Request) => Promise<LoginError | never>;
