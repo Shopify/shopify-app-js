1c7b1b6b1d8c6b5305a1d82bb43560aa86bd67e2
Richard Powell <richard.powell@shopify.com>
2023-07-24 16:39:27 -0400

    Update our types

--- packages/shopify-app-remix/README.md
+++ packages/shopify-app-remix/README.md
@@ -351,12 +351,14 @@ import {ActionArgs} from '@remix-run/node';
 import shopify from '../shopify.server';
 import db from '../db.server';
 
-export const action = async ({request}: ActionArgs) => {
-  const {topic, shop} = await shopify.authenticate.webhook(request);
+export const action = async ({request}) => {
+  const {topic, shop, session} = await authenticate.webhook(request);
 
   switch (topic) {
     case 'APP_UNINSTALLED':
-      await db.session.deleteMany({where: {shop}});
+      if (session) {
+        await db.session.deleteMany({where: {shop}});
+      }
       break;
     case 'CUSTOMERS_DATA_REQUEST':
     case 'CUSTOMERS_REDACT':
--- packages/shopify-app-remix/src/types.ts
+++ packages/shopify-app-remix/src/types.ts
@@ -289,11 +289,13 @@ export interface ShopifyAppBase<Config extends AppConfigArg> {
      * import db from "../db.server";
      *
      * export const action = async ({ request }: ActionArgs) => {
-     *   const { topic, shop, admin, payload } = await authenticate.webhook(request);
+     *   const { topic, shop, session } = await authenticate.webhook(request);
      *
      *   switch (topic) {
      *     case "APP_UNINSTALLED":
-     *       await db.session.deleteMany({ where: { shop } });
+     *       if (session) {
+     *         await db.session.deleteMany({ where: { shop } });
+     *       }
      *       break;
      *     case "CUSTOMERS_DATA_REQUEST":
      *     case "CUSTOMERS_REDACT":
