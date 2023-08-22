e7b8664c502fe6236dc2cd705656f12c60f33730
Richard Powell <richard.powell@shopify.com>
2023-08-22 09:45:47 -0400

    Fix failing tests due incorrect paths

--- packages/shopify-app-remix/src/server/auth/admin/billing/__tests__/request.test.ts
+++ packages/shopify-app-remix/src/server/auth/admin/billing/__tests__/request.test.ts
@@ -6,7 +6,7 @@ import {
   Shopify,
 } from '@shopify/shopify-api';
 
-import {shopifyApp} from '../..';
+import {shopifyApp} from '../../../..';
 import {
   APP_URL,
   BASE64_HOST,
@@ -19,12 +19,12 @@ import {
   setUpValidSession,
   signRequestCookie,
   testConfig,
-} from '../../__tests__/test-helper';
+} from '../../../../__tests__/test-helper';
 import {
   mockExternalRequest,
   mockExternalRequests,
-} from '../../__tests__/request-mock';
-import {REAUTH_URL_HEADER} from '../../auth/const';
+} from '../../../../__tests__/request-mock';
+import {REAUTH_URL_HEADER} from '../../../const';
 
 import * as responses from './mock-responses';
 
