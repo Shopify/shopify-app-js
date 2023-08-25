b22351bba8b78335ef23cc431194d1a6d8b31b82
Richard Powell <richard.powell@shopify.com>
2023-08-25 15:05:17 -0400

    Add tests for the admin client

--- packages/shopify-app-remix/src/server/authenticate/public/appProxy/__tests__/authenticate.test.ts
+++ packages/shopify-app-remix/src/server/authenticate/public/appProxy/__tests__/authenticate.test.ts
@@ -1,8 +1,6 @@
 import {HashFormat, createSHA256HMAC} from '@shopify/shopify-api/runtime';
-import {Session} from '@shopify/shopify-api';
-import {MemorySessionStorage} from '@shopify/shopify-app-session-storage-memory';
 
-import {shopifyApp} from '../../../..';
+import {LATEST_API_VERSION, shopifyApp} from '../../../..';
 import {
   API_SECRET_KEY,
   APP_URL,
@@ -11,6 +9,9 @@ import {
   setUpValidSession,
   testConfig,
 } from '../../../../__tests__/test-helper';
+import {mockExternalRequest} from '../../../../__tests__/request-mock';
+
+const REQUEST_URL = `https://${TEST_SHOP}/admin/api/${LATEST_API_VERSION}/customers.json`;
 
 describe('authenticating app proxy requests', () => {
   it('Throws a 400 response if there is no signature param', async () => {
@@ -132,8 +133,125 @@ describe('authenticating app proxy requests', () => {
   });
 
   describe('Returns the admin client', () => {
-    it('TODO', () => {
-      expect(false).toBe(true);
+    it('REST client can perform GET requests', async () => {
+      // GIVEN
+      const shopify = shopifyApp(testConfig());
+      await setUpValidSession(shopify.sessionStorage, false);
+      const {admin} = await shopify.unauthenticated.admin(TEST_SHOP);
+
+      await mockExternalRequest({
+        request: new Request(REQUEST_URL),
+        response: new Response(JSON.stringify({customers: []})),
+      });
+
+      // WHEN
+      const response = await admin.rest.get({path: 'customers'});
+
+      // THEN
+      expect(response.status).toEqual(200);
+      expect(await response.json()).toEqual({customers: []});
+    });
+
+    it('REST client can perform POST requests', async () => {
+      // GIVEN
+      const shopify = shopifyApp(testConfig());
+      await setUpValidSession(shopify.sessionStorage, false);
+      const {admin} = await shopify.unauthenticated.admin(TEST_SHOP);
+
+      await mockExternalRequest({
+        request: new Request(REQUEST_URL, {method: 'POST'}),
+        response: new Response(JSON.stringify({customers: []})),
+      });
+
+      // WHEN
+      const response = await admin.rest.post({
+        path: '/customers.json',
+        data: '',
+      });
+
+      // THEN
+      expect(response.status).toEqual(200);
+      expect(await response.json()).toEqual({customers: []});
+    });
+
+    it('REST client can perform PUT requests', async () => {
+      // GIVEN
+      const shopify = shopifyApp(testConfig());
+      await setUpValidSession(shopify.sessionStorage, false);
+      const {admin} = await shopify.unauthenticated.admin(TEST_SHOP);
+
+      await mockExternalRequest({
+        request: new Request(REQUEST_URL, {method: 'PUT'}),
+        response: new Response(JSON.stringify({customers: []})),
+      });
+
+      // WHEN
+      const response = await admin.rest.put({
+        path: '/customers.json',
+        data: '',
+      });
+
+      // THEN
+      expect(response.status).toEqual(200);
+      expect(await response.json()).toEqual({customers: []});
+    });
+
+    it('REST client can perform DELETE requests', async () => {
+      // GIVEN
+      const shopify = shopifyApp(testConfig());
+      await setUpValidSession(shopify.sessionStorage, false);
+      const {admin} = await shopify.unauthenticated.admin(TEST_SHOP);
+
+      await mockExternalRequest({
+        request: new Request(REQUEST_URL, {method: 'DELETE'}),
+        response: new Response(JSON.stringify({customers: []})),
+      });
+
+      // WHEN
+      const response = await admin.rest.delete({path: '/customers.json'});
+
+      // THEN
+      expect(response.status).toEqual(200);
+      expect(await response.json()).toEqual({customers: []});
+    });
+
+    it('GraphQL client can perform requests', async () => {
+      // GIVEN
+      const shopify = shopifyApp(testConfig());
+      const session = await setUpValidSession(shopify.sessionStorage, false);
+      const {admin} = await shopify.unauthenticated.admin(TEST_SHOP);
+
+      await mockExternalRequest({
+        request: new Request(
+          `https://${TEST_SHOP}/admin/api/${LATEST_API_VERSION}/graphql.json`,
+          {
+            method: 'POST',
+            headers: {'X-Shopify-Access-Token': session.accessToken!},
+          },
+        ),
+        response: new Response(JSON.stringify({shop: {name: 'Test shop'}})),
+      });
+
+      // WHEN
+      const response = await admin.graphql('{ shop { name } }');
+
+      // THEN
+      expect(response.status).toEqual(200);
+      expect(await response.json()).toEqual({shop: {name: 'Test shop'}});
+    });
+
+    it('returns a session object as part of the context', async () => {
+      // GIVEN
+      const shopify = shopifyApp(testConfig());
+      const session = await setUpValidSession(shopify.sessionStorage, false);
+
+      // WHEN
+      const {session: actualSession} = await shopify.unauthenticated.admin(
+        TEST_SHOP,
+      );
+
+      // THEN
+      expect(actualSession).toEqual(session);
     });
   });
 });
