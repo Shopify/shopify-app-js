04959fb8dce796ec7dc5c264b52671cf9c3ea712
Rezaan Syed <rezaan.syed@shopify.com>
2023-08-30 15:12:35 -0400

    Create new token-exchange file

--- packages/shopify-app-remix/src/auth/admin/token-exchange.ts
+++ packages/shopify-app-remix/src/auth/admin/token-exchange.ts
@@ -65,8 +65,12 @@ export class AuthStrategy<
 
   public async authenticateAdmin(
     request: Request,
-  ): Promise<AdminContext<Config, Resources>> {
+  ): Promise<EmbeddedAdminContext<Config, Resources>> {
     const {api, logger, config} = this;
+
+    if (!api.config.isEmbeddedApp) {
+      throw Error('You cannot use token exchange for a non embedded app')
+    }
 
     rejectBotRequest({api, logger, config}, request);
     respondToOptionsRequest({api, logger, config}, request);
@@ -84,23 +88,13 @@ export class AuthStrategy<
       throw errorOrResponse;
     }
 
-    const context:
-      | EmbeddedAdminContext<Config, Resources>
-      | NonEmbeddedAdminContext<Config, Resources> = {
+    return {
       admin: this.createAdminApiContext(request, sessionContext.session),
       billing: this.createBillingContext(request, sessionContext.session),
       session: sessionContext.session,
       cors,
+      sessionToken: sessionContext!.token!,
     };
-
-    if (config.isEmbeddedApp) {
-      return {
-        ...context,
-        sessionToken: sessionContext!.token!,
-      } as AdminContext<Config, Resources>;
-    } else {
-      return context as AdminContext<Config, Resources>;
-    }
   }
 
   private async authenticateAndGetSessionContext(
@@ -112,9 +106,6 @@ export class AuthStrategy<
 
     const isPatchSessionToken =
       url.pathname === config.auth.patchSessionTokenPath;
-    const isExitIframe = url.pathname === config.auth.exitIframePath;
-    const isAuthRequest = url.pathname === config.auth.path;
-    const isAuthCallbackRequest = url.pathname === config.auth.callbackPath;
     const sessionTokenHeader = getSessionTokenHeader(request);
 
     logger.info('Authenticating admin request');
@@ -122,15 +113,6 @@ export class AuthStrategy<
     if (isPatchSessionToken) {
       logger.debug('Rendering bounce page');
       throw this.renderAppBridge(request);
-    } else if (isExitIframe) {
-      const destination = url.searchParams.get('exitIframe')!;
-
-      logger.debug('Rendering exit iframe page', {destination});
-      throw this.renderAppBridge(request, destination);
-    } else if (isAuthCallbackRequest) {
-      throw await this.handleAuthCallbackRequest(request);
-    } else if (isAuthRequest) {
-      throw await this.handleAuthBeginRequest(request);
     } else if (sessionTokenHeader) {
       const sessionToken = await validateSessionToken(
         {api, logger, config},
@@ -151,138 +133,47 @@ export class AuthStrategy<
     }
   }
 
-  private async handleAuthBeginRequest(request: Request): Promise<never> {
-    const {api, config, logger} = this;
-
-    logger.info('Handling OAuth begin request');
-
-    const shop = this.ensureValidShopParam(request);
-
-    logger.debug('OAuth request contained valid shop', {shop});
-
-    // If we're loading from an iframe, we need to break out of it
-    if (
-      config.isEmbeddedApp &&
-      request.headers.get('Sec-Fetch-Dest') === 'iframe'
-    ) {
-      logger.debug('Auth request in iframe detected, exiting iframe', {shop});
-      throw redirectWithExitIframe({api, config, logger}, request, shop);
-    } else {
-      throw await beginAuth({api, config, logger}, request, false, shop);
-    }
-  }
-
-  private async handleAuthCallbackRequest(request: Request): Promise<never> {
-    const {api, config, logger} = this;
-
-    logger.info('Handling OAuth callback request');
-
-    const shop = this.ensureValidShopParam(request);
-
-    try {
-      const {session, headers: responseHeaders} = await api.auth.callback({
-        rawRequest: request,
-      });
-
-      await config.sessionStorage.storeSession(session);
-
-      if (config.useOnlineTokens && !session.isOnline) {
-        logger.info('Requesting online access token for offline session');
-        await beginAuth({api, config, logger}, request, true, shop);
-      }
-
-      if (config.hooks.afterAuth) {
-        logger.info('Running afterAuth hook');
-        await config.hooks.afterAuth({
-          session,
-          admin: this.createAdminApiContext(request, session),
-        });
-      }
-
-      throw await this.redirectToShopifyOrAppRoot(request, responseHeaders);
-    } catch (error) {
-      if (error instanceof Response) {
-        throw error;
-      }
-
-      logger.error('Error during OAuth callback', {error: error.message});
-
-      if (error instanceof CookieNotFound) {
-        throw await this.handleAuthBeginRequest(request);
-      } else if (
-        error instanceof InvalidHmacError ||
-        error instanceof InvalidOAuthError
-      ) {
-        throw new Response(undefined, {
-          status: 400,
-          statusText: 'Invalid OAuth Request',
-        });
-      } else {
-        throw new Response(undefined, {
-          status: 500,
-          statusText: 'Internal Server Error',
-        });
-      }
-    }
-  }
-
   private async validateUrlParams(request: Request) {
     const {api, config, logger} = this;
 
-    if (config.isEmbeddedApp) {
-      const url = new URL(request.url);
-      const shop = api.utils.sanitizeShop(url.searchParams.get('shop')!);
-      if (!shop) {
-        logger.debug('Missing or invalid shop, redirecting to login path', {
-          shop,
-        });
-        throw redirect(config.auth.loginPath);
-      }
-
-      const host = api.utils.sanitizeHost(url.searchParams.get('host')!);
-      if (!host) {
-        logger.debug('Invalid host, redirecting to login path', {
-          host: url.searchParams.get('host'),
-        });
-        throw redirect(config.auth.loginPath);
-      }
-    }
-  }
-
-  private ensureValidShopParam(request: Request): string {
     const url = new URL(request.url);
-    const {api} = this;
     const shop = api.utils.sanitizeShop(url.searchParams.get('shop')!);
-
     if (!shop) {
-      throw new Response('Shop param is invalid', {
-        status: 400,
+      logger.debug('Missing or invalid shop, redirecting to login path', {
+        shop,
       });
+      throw redirect(config.auth.loginPath);
     }
 
-    return shop;
+    const host = api.utils.sanitizeHost(url.searchParams.get('host')!);
+    if (!host) {
+      logger.debug('Invalid host, redirecting to login path', {
+        host: url.searchParams.get('host'),
+      });
+      throw redirect(config.auth.loginPath);
+    }
   }
 
   private async ensureAppIsEmbeddedIfRequired(request: Request) {
-    const {api, logger} = this;
+    const {logger} = this;
     const url = new URL(request.url);
 
     const shop = url.searchParams.get('shop')!;
 
-    if (api.config.isEmbeddedApp && url.searchParams.get('embedded') !== '1') {
+    if (url.searchParams.get('embedded') !== '1') {
       logger.debug('App is not embedded, redirecting to Shopify', {shop});
       await this.redirectToShopifyOrAppRoot(request);
     }
   }
 
   private async ensureSessionTokenSearchParamIfRequired(request: Request) {
-    const {api, logger} = this;
+    const {logger} = this;
     const url = new URL(request.url);
 
     const shop = url.searchParams.get('shop')!;
     const searchParamSessionToken = url.searchParams.get(SESSION_TOKEN_PARAM);
 
-    if (api.config.isEmbeddedApp && !searchParamSessionToken) {
+    if (!searchParamSessionToken) {
       logger.debug(
         'Missing session token in search params, going to bounce page',
         {shop},
@@ -298,35 +189,21 @@ export class AuthStrategy<
     const shop = url.searchParams.get('shop')!;
     const searchParamSessionToken = url.searchParams.get(SESSION_TOKEN_PARAM)!;
 
-    if (api.config.isEmbeddedApp) {
-      logger.debug(
-        'Session token is present in query params, validating session',
-        {shop},
-      );
+    logger.debug(
+      'Session token is present in query params, validating session',
+      {shop},
+    );
 
-      const sessionToken = await validateSessionToken(
-        {api, config, logger},
-        searchParamSessionToken,
-      );
+    const sessionToken = await validateSessionToken(
+      {api, config, logger},
+      searchParamSessionToken,
+    );
 
-      return this.getTokenViaTokenExchange(request, searchParamSessionToken, sessionToken);
-    } else {
-      // eslint-disable-next-line no-warning-comments
-      // TODO move this check into loadSession once we add support for it in the library
-      // https://github.com/orgs/Shopify/projects/6899/views/1?pane=issue&itemId=28378114
-      const sessionId = await api.session.getCurrentId({
-        isOnline: config.useOnlineTokens,
-        rawRequest: request,
-      });
-      if (!sessionId) {
-        logger.debug('Session id not found in cookies, redirecting to OAuth', {
-          shop,
-        });
-        throw await beginAuth({api, config, logger}, request, false, shop);
-      }
-
-      return {session: await this.loadSession(request, shop, sessionId)};
-    }
+    return this.getTokenViaTokenExchange(
+      request,
+      searchParamSessionToken,
+      sessionToken,
+    );
   }
 
   private async getTokenViaTokenExchange(
@@ -367,43 +244,12 @@ export class AuthStrategy<
     return {session, token: payload};
   }
 
-  private async loadSession(
-    request: Request,
-    shop: string,
-    sessionId: string,
-  ): Promise<Session> {
-    const {api, config, logger} = this;
-
-    logger.debug('Loading session from storage', {sessionId});
-
-    const session = await config.sessionStorage.loadSession(sessionId);
-    if (!session) {
-      logger.debug('No session found, redirecting to OAuth', {shop});
-      await redirectToAuthPage({api, config, logger}, request, shop);
-    } else if (!session.isActive(config.scopes)) {
-      logger.debug(
-        'Found a session, but it has expired, redirecting to OAuth',
-        {shop},
-      );
-      await redirectToAuthPage({api, config, logger}, request, shop);
-    }
-
-    return session!;
-  }
-
   private async redirectToShopifyOrAppRoot(
     request: Request,
     responseHeaders?: Headers,
   ): Promise<never> {
     const {api} = this;
-    const url = new URL(request.url);
-
-    const host = api.utils.sanitizeHost(url.searchParams.get('host')!)!;
-    const shop = api.utils.sanitizeShop(url.searchParams.get('shop')!)!;
-
-    const redirectUrl = api.config.isEmbeddedApp
-      ? await api.auth.getEmbeddedAppUrl({rawRequest: request})
-      : `/?shop=${shop}&host=${encodeURIComponent(host)}`;
+    const redirectUrl = await api.auth.getEmbeddedAppUrl({rawRequest: request});
 
     throw redirect(redirectUrl, {headers: responseHeaders});
   }
