db6f91c6c012b22daafeb477033144ed3e01defb
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-10-04 15:53:16 -0400

    Add documentation guide on future flags

--- packages/shopify-app-remix/src/server/authenticate/webhooks/authenticate.webhooks.doc.ts
+++ packages/shopify-app-remix/src/server/authenticate/webhooks/authenticate.webhooks.doc.ts
@@ -4,7 +4,7 @@ const data: ReferenceEntityTemplateSchema = {
   name: 'Webhook',
   description:
     'Contains functions for verifying Shopify webhooks.' +
-    '\n\n> Note: The format of the `admin` object returned by this function changes with the `v3_webhookAdminContext` future flag.',
+    '\n\n> Note: The format of the `admin` object returned by this function changes with the `v3_webhookAdminContext` future flag. Learn more about [gradual feature adoption](/docs/api/shopify-app-remix/guide-future-flags).',
   category: 'Authenticate',
   type: 'object',
   isVisualComponent: false,
