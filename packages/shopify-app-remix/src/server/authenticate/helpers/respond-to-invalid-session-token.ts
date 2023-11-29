8380cc769a6effe8139ce5f809d81ff951c83d60
Rachel Carvalho <rachel.carvalho@shopify.com>
2023-11-29 13:44:37 -0500

    add token exchange strategy

---
+++ packages/shopify-app-remix/src/server/authenticate/helpers/respond-to-invalid-session-token.ts
@@ -0,0 +1,21 @@
+import {BasicParams} from 'src/server/types';
+
+import {redirectToBouncePage} from '../admin/helpers/redirect-to-bounce-page';
+
+export function respondToInvalidSessionToken(
+  params: BasicParams,
+  request: Request,
+) {
+  const {api, logger, config} = params;
+
+  const isDocumentRequest = !request.headers.get('authorization');
+  if (isDocumentRequest) {
+    return redirectToBouncePage({api, logger, config}, new URL(request.url));
+  }
+
+  // TODO add header so app bridge retries with new session token
+  throw new Response(undefined, {
+    status: 401,
+    statusText: 'Unauthorized',
+  });
+}
