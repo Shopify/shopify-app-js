9b4ace81c06d22d351c30bb3c23a7d5cbd6c40ce
Rachel Carvalho <rachel.carvalho@shopify.com>
2023-11-30 12:21:41 -0500

    add tests to token exchange strategy

--- packages/shopify-app-remix/src/server/__test-helpers/setup-valid-session.ts
+++ packages/shopify-app-remix/src/server/__test-helpers/setup-valid-session.ts
@@ -6,13 +6,14 @@ import {TEST_SHOP, USER_ID} from './const';
 export async function setUpValidSession(
   sessionStorage: SessionStorage,
   isOnline = false,
+  expires: Date | undefined = undefined,
 ): Promise<Session> {
   const overrides: Partial<Session> = {};
   let id = `offline_${TEST_SHOP}`;
   if (isOnline) {
     id = `${TEST_SHOP}_${USER_ID}`;
     // Expires one day from now
-    overrides.expires = new Date(Date.now() + 1000 * 3600 * 24);
+    overrides.expires = expires || new Date(Date.now() + 1000 * 3600 * 24);
     overrides.onlineAccessInfo = {
       associated_user_scope: 'testScope',
       expires_in: 3600 * 24,
