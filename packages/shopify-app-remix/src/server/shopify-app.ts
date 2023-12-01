39e40f8304e0c18810157303bd95fcaee2a53bb2
Rezaan Syed <rezaan.syed@shopify.com>
2023-12-01 16:07:41 -0500

    Update types to account for unstable_tokenExchange and unstable_newEmbeddedAuthStrategy future flags

--- packages/shopify-app-remix/src/server/shopify-app.ts
+++ packages/shopify-app-remix/src/server/shopify-app.ts
@@ -71,7 +71,7 @@ export function shopifyApp<
   const authStrategy = authStrategyFactory<Config, Resources>({
     ...params,
     strategy:
-      config.future.unstable_tokenExchange && config.isEmbeddedApp
+      config.future.unstable_newEmbeddedAuthStrategy && config.isEmbeddedApp
         ? tokenExchange
         : oauth,
   });
@@ -152,7 +152,10 @@ function deriveApi(appConfig: AppConfigArg) {
     isEmbeddedApp: appConfig.isEmbeddedApp ?? true,
     apiVersion: appConfig.apiVersion ?? LATEST_API_VERSION,
     isCustomStoreApp: appConfig.distribution === AppDistribution.ShopifyAdmin,
-    future: {},
+    future: {
+      unstable_tokenExchange:
+        appConfig.future?.unstable_newEmbeddedAuthStrategy,
+    },
   });
 }
 
