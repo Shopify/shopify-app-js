97cc308d5940ac93c0d540ff5b488c1a5d74f103
Rezaan Syed <rezaan.syed@shopify.com>
2023-12-11 15:57:15 -0500

    Add error handling for token exchange strategy on invalid API requests

--- packages/shopify-app-remix/src/server/authenticate/const.ts
+++ packages/shopify-app-remix/src/server/authenticate/const.ts
@@ -4,6 +4,10 @@ export const APP_BRIDGE_URL =
 export const REAUTH_URL_HEADER =
   'X-Shopify-API-Request-Failure-Reauthorize-Url';
 
+export const RETRY_INVALID_SESSION_HEADER = {
+  'X-Shopify-Retry-Invalid-Session-Request': '1',
+};
+
 export const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Authorization',
