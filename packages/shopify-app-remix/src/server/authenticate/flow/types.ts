9d77d99ea43b5c3f76309770735afbe5005f9b7f
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2024-02-07 14:59:35 -0500

    Adding documentation for flow authentication

--- packages/shopify-app-remix/src/server/authenticate/flow/types.ts
+++ packages/shopify-app-remix/src/server/authenticate/flow/types.ts
@@ -5,8 +5,83 @@ import type {AdminApiContext} from '../../clients';
 export interface FlowContext<
   Resources extends ShopifyRestResources = ShopifyRestResources,
 > {
+  /**
+   * A session with an offline token for the shop.
+   *
+   * Returned only if there is a session for the shop.
+   *
+   * @example
+   * <caption>Shopify session for the Flow request.</caption>
+   * <description>Use the session associated with this request to use REST resources.</description>
+   * ```ts
+   * // /app/routes/flow.tsx
+   * import { ActionFunction } from "@remix-run/node";
+   * import { authenticate } from "../shopify.server";
+   *
+   * export const action: ActionFunction = async ({ request }) => {
+   *   const { session, admin } = await authenticate.flow(request);
+   *
+   *   const products = await admin?.rest.resources.Product.all({ session });
+   *   // Use products
+   *
+   *   return new Response();
+   * };
+   * ```
+   */
   session: Session;
+
+  /**
+   * The payload from the Flow request.
+   *
+   * @example
+   * <caption>Flow payload.</caption>
+   * <description>Get the request's POST payload.</description>
+   * ```ts
+   * // /app/routes/flow.tsx
+   * import { ActionFunction } from "@remix-run/node";
+   * import { authenticate } from "../shopify.server";
+   *
+   * export const action: ActionFunction = async ({ request }) => {
+   *   const { payload } = await authenticate.flow(request);
+   *   return new Response();
+   * };
+   * ```
+   */
   payload: any;
+
+  /**
+   * An admin context for the Flow request.
+   *
+   * Returned only if there is a session for the shop.
+   *
+   * @example
+   * <caption>Flow admin context.</caption>
+   * <description>Use the `admin` object in the context to interact with the Admin API.</description>
+   * ```ts
+   * // /app/routes/flow.tsx
+   * import { ActionFunctionArgs } from "@remix-run/node";
+   * import { authenticate } from "../shopify.server";
+   *
+   * export async function action({ request }: ActionFunctionArgs) {
+   *   const { admin } = await authenticate.flow(request);
+   *
+   *   const response = await admin?.graphql(
+   *     `#graphql
+   *     mutation populateProduct($input: ProductInput!) {
+   *       productCreate(input: $input) {
+   *         product {
+   *           id
+   *         }
+   *       }
+   *     }`,
+   *     { variables: { input: { title: "Product Name" } } }
+   *   );
+   *
+   *   const productData = await response.json();
+   *   return json({ data: productData.data });
+   * }
+   * ```
+   */
   admin: AdminApiContext<Resources>;
 }
 
