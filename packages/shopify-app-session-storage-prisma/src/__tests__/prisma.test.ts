9ac8bb66657e23555cd1db6b0f01fcad517dc4d0
Zoey Lan <zoey.lan@shopify.com>
2024-03-06 14:26:55 -0700

    Add test for handling Prisma P2002 error

--- packages/shopify-app-session-storage-prisma/src/__tests__/prisma.test.ts
+++ packages/shopify-app-session-storage-prisma/src/__tests__/prisma.test.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import {execSync} from 'child_process';
 
+import {Session} from '@shopify/shopify-api';
 import {batteryOfTests} from '@shopify/shopify-app-session-storage-test-utils';
-import {PrismaClient} from '@prisma/client';
+import {Prisma, PrismaClient} from '@prisma/client';
 
 import {MissingSessionTableError, PrismaSessionStorage} from '../prisma';
 
@@ -55,6 +56,50 @@ describe('PrismaSessionStorage when with no database set up', () => {
         'The table `main.Session` does not exist in the current database.',
       );
     }
+  });
+});
+
+describe('Prisma throws P2002 on upsert', () => {
+  let prisma: PrismaClient;
+  let storage: PrismaSessionStorage<PrismaClient>;
+  let session: Session;
+
+  beforeAll(async () => {
+    // Reset the database prior to the tests
+    clearTestDatabase();
+
+    execSync('npx prisma migrate dev --name init --preview-feature');
+
+    prisma = new PrismaClient();
+    storage = new PrismaSessionStorage<PrismaClient>(prisma);
+    session = new Session({
+      id: 'session-123',
+      shop: 'shop',
+      state: 'state',
+      isOnline: false,
+      accessToken: '123',
+      scope: '',
+    });
+
+    jest.spyOn(prisma.session, 'upsert').mockImplementation(() => {
+      throw new Prisma.PrismaClientKnownRequestError('error message', {
+        code: 'P2002',
+      });
+    });
+  });
+
+  afterAll(async () => {
+    await prisma.session.deleteMany();
+    jest.clearAllMocks();
+  });
+
+  it('handles Prisma P2002 errors', async () => {
+    await expect(storage.storeSession(session)).resolves.not.toThrow();
+  });
+
+  it('Returns true after handling Prisma P2002 errors', async () => {
+    const result = await storage.storeSession(session);
+    expect(result).toBe(true);
   });
 });
 
