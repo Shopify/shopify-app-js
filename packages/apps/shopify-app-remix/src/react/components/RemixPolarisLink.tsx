1a1fc1551e2d6bba8d71f2a44857569f6f748392
Henry Tao <henry.tao@shopify.com>
2024-04-23 14:57:37 -0400

    Fix back stack for ABN app

--- packages/apps/shopify-app-remix/src/react/components/RemixPolarisLink.tsx
+++ packages/apps/shopify-app-remix/src/react/components/RemixPolarisLink.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Link} from '@remix-run/react';
+import React, {useEffect, useRef, useState} from 'react';
+import {Link, useNavigate} from '@remix-run/react';
 // Polaris' LinkProps doesn't match this type, so we need to import it directly
 import type {
   LinkLikeComponent,
@@ -9,6 +9,83 @@ import type {
 export const RemixPolarisLink = React.forwardRef<
   HTMLAnchorElement,
   LinkLikeComponentProps
->((props, ref) => (
-  <Link {...props} to={props.url ?? props.to} ref={ref} />
-)) as LinkLikeComponent;
+>((props, forwardedRef) => {
+  const [element, setElement] = useState<HTMLElement>();
+  const navigate = useNavigate();
+  const navigateRef = useRef(navigate);
+  navigateRef.current = navigate;
+
+  useEffect(() => {
+    async function onClick(event: MouseEvent) {
+      const isAnchorTag = event.target instanceof HTMLAnchorElement;
+      if (!hasAppBridgeCDN() || !isAnchorTag) return;
+
+      const anchor = event.target;
+      const url = parseUrl(anchor.href);
+      if (!url || url.origin !== window.location.origin) return;
+
+      const leaveConfirmation = getLeaveConfirmation();
+      if (!leaveConfirmation) return;
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      await leaveConfirmation().catch(noop);
+
+      const {pathname, search, hash} = url;
+      navigateRef.current(`${pathname}${search}${hash}`);
+    }
+    element?.addEventListener('click', onClick);
+    return () => {
+      element?.removeEventListener('click', onClick);
+    };
+  }, [element]);
+
+  return (
+    <Link
+      {...props}
+      to={props.url ?? props.to}
+      ref={(anchor) => {
+        setElement(anchor || undefined);
+        if (forwardedRef) {
+          if (typeof forwardedRef === 'function') {
+            forwardedRef(anchor);
+          } else {
+            forwardedRef.current = anchor;
+          }
+        }
+      }}
+    />
+  );
+}) as LinkLikeComponent;
+
+function parseUrl(url: string | undefined | null) {
+  try {
+    if (!url) return null;
+    return new URL(url, window.location.origin);
+  } catch (_) {
+    return null;
+  }
+}
+
+function hasAppBridgeCDN() {
+  return 'shopify' in window;
+}
+
+const noop = () => {};
+
+/**
+ * Ref https://shopify.dev/docs/api/app-bridge-library/apis/navigation
+ */
+function getLeaveConfirmation(): () => Promise<void> {
+  try {
+    const shopify: any = 'shopify' in window ? window.shopify : undefined;
+    const {leaveConfirmation} = shopify?.saveBar || {};
+    if (typeof leaveConfirmation === 'function') {
+      return () => leaveConfirmation();
+    }
+  } catch (_) {
+    // Noop
+  }
+  return () => Promise.resolve();
+}
