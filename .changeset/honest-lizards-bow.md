fa94e857dd72911dbbf3c97a5c51c9c3e5d946e8
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2024-04-25 14:08:01 -0400

    Don't consider Shopify POS / Mobile requests as bots

---
+++ .changeset/honest-lizards-bow.md
@@ -0,0 +1,5 @@
+---
+"@shopify/shopify-app-remix": patch
+---
+
+Fix an issue that rejected requests from Shopify POS / Mobile due to their User-Agents being labeled as bots.
--- packages/apps/shopify-app-remix/src/server/authenticate/helpers/reject-bot-request.ts
+++ packages/apps/shopify-app-remix/src/server/authenticate/helpers/reject-bot-request.ts
@@ -2,11 +2,29 @@ import {isbot} from 'isbot';
 
 import type {BasicParams} from '../../types';
 
+const SHOPIFY_POS_USER_AGENT = /Shopify POS\/[0-9]+\.[0-9]+\.[0-9]+\/Android/;
+const SHOPIFY_MOBILE_ANDROID_USER_AGENT =
+  /Shopify Mobile\/Android\/[0-9]+\.[0-9]+\.[0-9]+/;
+
+const SHOPIFY_USER_AGENTS = [
+  SHOPIFY_POS_USER_AGENT,
+  SHOPIFY_MOBILE_ANDROID_USER_AGENT,
+];
+
 export function respondToBotRequest(
   {logger}: BasicParams,
   request: Request,
 ): void | never {
-  if (isbot(request.headers.get('User-Agent'))) {
+  const userAgent = request.headers.get('User-Agent') ?? '';
+
+  // We call isbot below to prevent good (self-identifying) bots from triggering auth requests, but there are some
+  // Shopify-specific cases we want to allow that are identified as bots by isbot.
+  if (SHOPIFY_USER_AGENTS.some((agent) => agent.test(userAgent))) {
+    logger.debug('Request is from a Shopify agent, allow');
+    return;
+  }
+
+  if (isbot(userAgent)) {
     logger.debug('Request is from a bot, skipping auth');
     throw new Response(undefined, {status: 410, statusText: 'Gone'});
   }
