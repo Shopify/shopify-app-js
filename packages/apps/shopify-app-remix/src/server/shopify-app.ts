71efd02a2ae2e016cfd7427b52ec60011859a0e1
poetry by <2638986+btomaj@users.noreply.github.com>
2024-07-14 09:28:26 +1000

    Linting

--- packages/apps/shopify-app-remix/src/server/shopify-app.ts
+++ packages/apps/shopify-app-remix/src/server/shopify-app.ts
@@ -65,10 +65,7 @@ export function shopifyApp<
   Future extends FutureFlagOptions = Config['future'],
 >(appConfig: Readonly<Config>): ShopifyApp<Config> {
   if (appConfig.isTesting === true) {
-    appConfig = {
-      ...appConfig,
-      ...testConfig(),
-    };
+    Object.assign(appConfig, testConfig());
   }
 
   const api = deriveApi(appConfig);
@@ -200,13 +197,6 @@ function deriveConfig<Storage extends SessionStorage>(
 
   const authPathPrefix = appConfig.authPathPrefix || '/auth';
   appConfig.distribution = appConfig.distribution ?? AppDistribution.AppStore;
-
-  if (appConfig.isTesting === true) {
-    appConfig = {
-      ...appConfig,
-      ...testConfig(),
-    };
-  }
 
   return {
     ...appConfig,
