9ecb63d12db0b22c727d78c15a6c30e166693f66
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2024-07-22 15:04:21 -0400

    Updating remix package docs

--- .changeset/perfect-countries-tickle.md
+++ .changeset/perfect-countries-tickle.md
@@ -2,8 +2,8 @@
 '@shopify/shopify-api': minor
 ---
 
-Added a new future flag `unstable_managedPricingSupport`, which will:
+Added a new future flag `unstable_managedPricingSupport` to support apps using [Shopify managed pricing](https://shopify.dev/docs/apps/launch/billing/managed-pricing), which will:
 
 - Change `billing.check` to always return an object.
 - Change `billing.check` and `billing.subscription` to work without a billing config.
-- Allow finding charges for apps using [Shopify managed pricing](https://shopify.dev/docs/apps/launch/billing/managed-pricing) by calling the billing `check` function without a `plans` filter.
+- Allow calling charges with `billing.check` without a `plans` filter. The `hasActivePayment` value will be true if any purchases are found with the given filters.
--- packages/apps/shopify-app-remix/src/server/authenticate/admin/billing/types.ts
+++ packages/apps/shopify-app-remix/src/server/authenticate/admin/billing/types.ts
@@ -184,8 +184,8 @@ export interface BillingContext<Config extends AppConfigArg> {
    *     plans: [MONTHLY_PLAN],
    *     isTest: false,
    *   });
-   *  console.log(hasActivePayment)
-   *  console.log(appSubscriptions)
+   *   console.log(hasActivePayment);
+   *   console.log(appSubscriptions);
    * };
    * ```
    * ```ts
@@ -218,6 +218,23 @@ export interface BillingContext<Config extends AppConfigArg> {
    * export const authenticate = shopify.authenticate;
    * ```
    *
+   * @example
+   * <caption>Check for payments without filtering.</caption>
+   * <description>Use billing.check to see if any payments exist for the store, regardless of whether it's a test or
+   * matches one or more plans.</description>
+   * ```ts
+   * // /app/routes/**\/*.ts
+   * import { LoaderFunctionArgs } from "@remix-run/node";
+   * import { authenticate, MONTHLY_PLAN } from "../shopify.server";
+   *
+   * export const loader = async ({ request }: LoaderFunctionArgs) => {
+   *   const { billing } = await authenticate.admin(request);
+   *   const { hasActivePayment, appSubscriptions } = await billing.check();
+   *   // This will be true if any payment is found
+   *   console.log(hasActivePayment);
+   *   console.log(appSubscriptions);
+   * };
+   * ```
    */
   check: <Options extends CheckBillingOptions<Config>>(
     options?: Options,
