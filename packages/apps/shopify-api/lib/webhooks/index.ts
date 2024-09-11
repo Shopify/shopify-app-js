bc5d3320f4992d2b472e6f25ccf0aeeab1a5a250
Richard Powell <richard.powell@shopify.com>
2024-09-11 15:59:06 -0400

    Add some TSDoc comments to the Webhook methods to try to steer partners towards app-specific webhooks

--- packages/apps/shopify-api/lib/webhooks/index.ts
+++ packages/apps/shopify-api/lib/webhooks/index.ts
@@ -6,7 +6,80 @@ import {process} from './process';
 import {validateFactory} from './validate';
 import {HttpWebhookHandlerWithCallback, WebhookRegistry} from './types';
 
-export function shopifyWebhooks(config: ConfigInterface) {
+
+interface Webhooks {
+  /**
+   * Add shop-specific webhook handlers to the library registry,
+   * allowing you to register webhooks with Shopify and process HTTP webhook requests from Shopify.
+   * Unless your app needs different webhooks for different shops, we recommend using app-specific webhooks:
+   *
+   * {@link https://shopify.dev/docs/apps/build/webhooks/subscribe#app-specific-subscriptions}
+   *
+   * If you use only app-specific webhooks, you do not need to use `addHandlers`.
+   *
+   */
+  addHandlers: ReturnType<typeof addHandlers>;
+
+  /**
+   * Fetches the configured handlers for shop-specific webhooks for the given topic.
+   *
+   * Unless your app needs different webhooks for different shops, we recommend using app-specific webhooks:
+   *
+   * {@link https://shopify.dev/docs/apps/build/webhooks/subscribe#app-specific-subscriptions}
+   *
+   * If you use only app-specific webhooks, you do not need to use `getTopicsAdded`.
+   *
+   */
+  getTopicsAdded: ReturnType<typeof getTopicsAdded>;
+
+  /**
+   * Fetches all topics that were added to the registry.
+   *
+   * Unless your app needs different webhooks for different shops, we recommend using app-specific webhooks:
+   *
+   * {@link https://shopify.dev/docs/apps/build/webhooks/subscribe#app-specific-subscriptions}
+   *
+   * If you use only app-specific webhooks, you do not need to use `getHandlers`.
+   *
+   */
+  getHandlers: ReturnType<typeof getHandlers>;
+
+  /**
+   * Registers a webhook handler for a given topic.
+   *
+   * Unless your app needs different webhooks for different shops, we recommend using app-specific webhooks:
+   *
+   * {@link https://shopify.dev/docs/apps/build/webhooks/subscribe#app-specific-subscriptions}
+   *
+   * If you use only app-specific webhooks, you do not need to use `register`.
+   *
+   */
+  register: ReturnType<typeof register>;
+
+  /**
+   * Processes a webhook request.
+   *
+   * Unless your app needs different webhooks for different shops, we recommend using app-specific webhooks:
+   *
+   * {@link https://shopify.dev/docs/apps/build/webhooks/subscribe#app-specific-subscriptions}
+   *
+   * If you use only app-specific webhooks, you do not need to use `process`.
+   *
+   */
+  process: ReturnType<typeof process>;
+
+  /**
+   * Validates an incoming request for `Http` handlers.
+   *
+   * If the call is invalid, it will return a `valid` field set to `false`.
+   *
+   * `validate` can be used to validate all Shopify webhook requests, regardless of if they are app-specific or shop-specific.
+   *
+   */
+  validate: ReturnType<typeof validateFactory>;
+}
+
+export function shopifyWebhooks(config: ConfigInterface): Webhooks {
   const webhookRegistry = registry();
 
   return {
