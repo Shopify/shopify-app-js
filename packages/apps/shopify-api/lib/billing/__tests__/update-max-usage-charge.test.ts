99a50545e28f364e858eb6ed42e482172279dad0
Andy Liu <andy.liu@shopify.com>
2024-10-07 14:37:54 -0400

    additional unit test

--- packages/apps/shopify-api/lib/billing/__tests__/update-max-usage-charge.test.ts
+++ packages/apps/shopify-api/lib/billing/__tests__/update-max-usage-charge.test.ts
@@ -3,6 +3,7 @@ import {
   BillingConfigUsageLineItem,
   BillingError,
   BillingInterval,
+  LATEST_API_VERSION,
   shopifyApi,
 } from '../..';
 import {TEST_FUTURE_FLAGS, testConfig} from '../../__tests__/test-config';
@@ -13,6 +14,12 @@ import * as Responses from './responses';
 
 const DOMAIN = 'test-shop.myshopify.io';
 const ACCESS_TOKEN = 'access-token';
+const GRAPHQL_BASE_REQUEST = {
+  method: 'POST',
+  domain: DOMAIN,
+  path: `/admin/api/${LATEST_API_VERSION}/graphql.json`,
+  headers: {'X-Shopify-Access-Token': ACCESS_TOKEN},
+};
 
 describe('shopify.billing.updateMaxUsageCharge', () => {
   const session = new Session({
@@ -49,6 +56,16 @@ describe('shopify.billing.updateMaxUsageCharge', () => {
     expect(response).toEqual(
       Responses.APP_SUBSCRIPTION_LINE_ITEM_UPDATE_PAYLOAD,
     );
+
+    expect({
+      ...GRAPHQL_BASE_REQUEST,
+      data: {
+        query: expect.stringContaining('appSubscriptionLineItemUpdate'),
+        variables: expect.objectContaining({
+          cappedAmount: {amount: 100, currencyCode: 'USD'},
+        }),
+      },
+    }).toMatchMadeHttpRequest();
   });
 
   it('throws a BillingError when no billing config is set', async () => {
@@ -57,6 +74,19 @@ describe('shopify.billing.updateMaxUsageCharge', () => {
     const response = shopify.billing.updateMaxUsageCharge({
       session,
       subscriptionLineItemId: '1234',
+      cappedAmount: {amount: 100, currencyCode: 'USD'},
+    });
+
+    expect(response).rejects.toThrow(BillingError);
+  });
+
+  it('throws a BillingError when an error occurs in the GraphQL query', async () => {
+    const shopify = shopifyApi(testConfig({billing}));
+    queueMockResponses([Responses.MAX_USAGE_CHARGE_UPDATE_RESPONSE_ERROR]);
+
+    const response = shopify.billing.updateMaxUsageCharge({
+      session,
+      subscriptionLineItemId: Responses.USAGE_CHARGE_SUBSCRIPTION_ID,
       cappedAmount: {amount: 100, currencyCode: 'USD'},
     });
 
