d01bb47ea494f6bb5710b6889eccef312a270e0d
Richard Powell <richard.powell@shopify.com>
2024-11-28 14:46:44 -0500

    Spike what doing token exchange in a webhook might look like

--- packages/apps/shopify-app-remix/src/server/authenticate/webhooks/authenticate.ts
+++ packages/apps/shopify-app-remix/src/server/authenticate/webhooks/authenticate.ts
@@ -1,4 +1,8 @@
 import {
+  InvalidJwtError,
+  InvalidShopError,
+  RequestedTokenType,
+  Session,
   ShopifyRestResources,
   WebhookValidationErrorReason,
 } from '@shopify/shopify-api';
@@ -6,8 +10,7 @@ import {
 import {AppConfigArg} from '../../config-types';
 import type {BasicParams} from '../../types';
 import {adminClientFactory} from '../../clients';
-import {handleClientErrorFactory} from '../admin/helpers';
-import {createOrLoadOfflineSession} from '../helpers';
+import {createOrLoadOfflineSession, getSessionTokenHeader} from '../helpers';
 
 import type {
   AuthenticateWebhook,
@@ -55,7 +58,35 @@ export function authenticateWebhookFactory<
         throw new Response(undefined, {status: 400, statusText: 'Bad Request'});
       }
     }
-    const session = await createOrLoadOfflineSession(check.domain, params);
+
+    let session = await createOrLoadOfflineSession(check.domain, params);
+
+    /**
+     * Most of the time the session should be in the database.
+     * There are two exceptions:
+     *
+     * 1) DB issues
+     * Let's say the developer dropped a table, or the DB is not available.
+     * This is an error state that's recoverable using token exchange.
+     *
+     * 2) SHOP_REDACT
+     * The app_uninstall webhook fired, the app deleted it's session,
+     * shop_redact fired up to 48 hours later.
+     * This is a legitimate situation, not an error state.
+     * It's not recoverable using token exchange since the app is uninstalled.
+     */
+    if (!session && check.topic !== 'SHOP_REDACT') {
+      const idToken = getSessionTokenHeader(request);
+
+      if (idToken) {
+        session = await getSessionFromTokenExchange(
+          params,
+          idToken,
+          check.domain,
+        );
+      }
+    }
+
     const webhookContext: WebhookContextWithoutSession<Topics> = {
       apiVersion: check.apiVersion,
       shop: check.domain,
@@ -67,20 +98,62 @@ export function authenticateWebhookFactory<
       admin: undefined,
     };
 
-    if (!session) {
-      return webhookContext;
+    if (session) {
+      return {
+        ...webhookContext,
+        session,
+        admin: adminClientFactory<ConfigArg, Resources>({
+          params,
+          session,
+        }),
+      };
     }
 
-    const admin = adminClientFactory<ConfigArg, Resources>({
-      params,
-      session,
-      handleClientError: handleClientErrorFactory({request}),
+    return webhookContext;
+  };
+}
+
+async function getSessionFromTokenExchange(
+  params: BasicParams,
+  idToken: string,
+  shop: string,
+): Promise<Session | undefined> {
+  const {api, config} = params;
+
+  try {
+    const {session} = await api.auth.tokenExchange({
+      shop,
+      sessionToken: idToken,
+      requestedTokenType: RequestedTokenType.OfflineAccessToken,
     });
 
-    return {
-      ...webhookContext,
-      session,
-      admin,
-    };
-  };
+    /**
+     * We may be in this code path because the DB has availability issues
+     *
+     * By catching we prevent the same DB issues from rejecting the request
+     * and the Remix action that called this code will still get Webhook context.
+     * There just won't be a session and that's ok, since webhooks can fire after uninstall.
+     * */
+    try {
+      await config.sessionStorage!.storeSession(session);
+    } catch (error) {
+      api.logger.error('Failed to store session after token exchange', {error});
+    }
+
+    return session;
+  } catch (error) {
+    /**
+     * InvalidJwtError or InvalidShopError means the request can't be trusted
+     * api.webhooks.validate() should reject any request that would trigger
+     * a InvalidJwtError or InvalidShopError. So this is just a belts and braces.
+     * If any other error is thrown, it's ok, we just can't create a new session.
+     * That's ok since webhooks can fire after the app is uninstalled.
+     */
+    if (error instanceof InvalidJwtError || error instanceof InvalidShopError) {
+      api.logger.error('Webhook validation failed. Invalid JWT', {error});
+      throw new Response(undefined, {status: 400, statusText: 'Bad Request'});
+    }
+
+    return undefined;
+  }
 }
