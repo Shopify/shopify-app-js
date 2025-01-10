f0a0ba2f4eace55215770b42ca187437a8e2a802
Rick Sperko <rick.sperko@shopify.com>
2025-01-10 15:07:49 -0600

    Add documentation for supporting Metafields in webhooks

--- packages/apps/shopify-app-remix/docs/staticPages/examples/guides/webhooks/declarative-config.example.ts
+++ packages/apps/shopify-app-remix/docs/staticPages/examples/guides/webhooks/declarative-config.example.ts
@@ -1,7 +1,19 @@
 [webhooks]
-api_version = "2024-04"
+api_version = "2025-04"
 
   [[webhooks.subscriptions]]
   topics = [ "app/uninstalled" ]
   uri = "/webhooks"
   compliance_topics = [ "customers/data_request", "customers/redact", "shop/redact" ]
+
+  [[webhooks.subscriptions]]
+  topics = [ "products/update" ]
+  uri = "https://example.org/endpoint"
+
+    [[webhooks.subscriptions.metafields]]
+    namespace = "custom"
+    key = "ingredients"
+
+    [[webhooks.subscriptions.metafields]]
+    namespace = "$app:product_highlight"
+    key = "title"
