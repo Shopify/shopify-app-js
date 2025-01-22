cb33f462fb8753fdb2734f567d4f8abed3cc1154
McTom234 <mc_tom234@gmx.de>
2025-01-22 13:52:51 +0100

    fix(shopify-api): 301 redirect follows with initial method

--- packages/apps/shopify-api/adapters/node/index.ts
+++ packages/apps/shopify-api/adapters/node/index.ts
@@ -21,8 +21,28 @@ import {
   nodeRuntimeString,
 } from './adapter';
 
-// For the purposes of this package, fetch correctly implements everything we need
-setAbstractFetchFunc(fetch as any as AbstractFetchFunc);
+// node-fetch redirects post requests as get requests on 301, 302 or 303 status codes
+// this does not work for graphql requests, so we need to manually handle redirects
+const fetchWithRedirect: AbstractFetchFunc = async (url, init) => {
+  const fetchOptions = {
+    ...init,
+    redirect: 'manual',
+  } satisfies RequestInit;
+
+  const response = await (fetch as any as AbstractFetchFunc)(url, fetchOptions);
+
+  if (
+    (response.status === 301 || response.status === 302) &&
+    response.headers.has('location')
+  ) {
+    const location = response.headers.get('location')!;
+    return fetchWithRedirect(location, init);
+  }
+
+  return response;
+};
+
+setAbstractFetchFunc(fetchWithRedirect);
 setAbstractConvertRequestFunc(nodeConvertRequest);
 setAbstractConvertIncomingResponseFunc(nodeConvertIncomingResponse);
 setAbstractConvertResponseFunc(nodeConvertAndSendResponse);
