ab0e770ef1836cfa16663607b44b8084658f46fc
Liz Kenyon <elizabeth.kenyon@shopify.com>
2025-06-18 14:33:48 -0500

    Remove non-embedded test infrastructure and merchant-custom test files

--- packages/apps/shopify-app-react-router/src/server/__test-helpers/index.ts
+++ packages/apps/shopify-app-react-router/src/server/__test-helpers/index.ts
@@ -17,4 +17,3 @@ export * from './get-thrown-error';
 export * from './setup-embedded-flow';
 export * from './setup-fetch-flow';
 export * from './mock-graphql-request';
-export * from './setup-non-embedded-flow';
