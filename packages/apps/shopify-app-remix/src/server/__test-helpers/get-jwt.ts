97ebe8f152ae9f5559444ef08cafb5ca0aaeef4a
Richard Powell <richard.powell@shopify.com>
2025-09-02 09:19:45 -0400

    Remix: Swap jsonwebtoken for jose, require Node >=20

--- packages/apps/shopify-app-remix/src/server/__test-helpers/get-jwt.ts
+++ packages/apps/shopify-app-remix/src/server/__test-helpers/get-jwt.ts
@@ -3,7 +3,7 @@ import {getJwt as getJwtImport} from '@shopify/shopify-api/test-helpers';
 
 import {API_KEY, API_SECRET_KEY, TEST_SHOP_NAME} from './const';
 
-export function getJwt(
+export async function getJwt(
   overrides: Partial<JwtPayload> = {},
 ): ReturnType<typeof getJwtImport> {
   return getJwtImport(TEST_SHOP_NAME, API_KEY, API_SECRET_KEY, overrides);
