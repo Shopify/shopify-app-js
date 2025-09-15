384fa42b09a52fa93b3256721a1e6e8979644503
Richard Powell <richard.powell@shopify.com>
2025-09-15 15:17:57 -0400

    Remix: Clean up exported types, the same way we did with the RR package

--- packages/apps/shopify-app-remix/src/server/types-contexts.ts
+++ packages/apps/shopify-app-remix/src/server/types-contexts.ts
@@ -2,62 +2,43 @@
 
 import type {AppConfigArg} from './config-types';
 import type {ShopifyApp} from './types';
-import type {
-  AdminContext as IAdminContext,
-  ScopesContext as IScopesContext,
-} from './authenticate/admin/types';
-import type {ScopesDetail as IScopesDetail} from './authenticate/admin/scope/types';
-import type {UnauthenticatedAdminContext as IUnauthenticatedAdminContext} from './unauthenticated/admin/types';
-import type {UnauthenticatedStorefrontContext as IUnauthenticatedStorefrontContext} from './unauthenticated/storefront/types';
+import type {AdminContext as IAdminContext} from './authenticate/admin/types';
 import type {WebhookContext as IWebhookContext} from './authenticate/webhooks/types';
-import type {FlowContext as IFlowContext} from './authenticate/flow/types';
-import type {FulfillmentServiceContext as IFulfillmentServiceContext} from './authenticate/fulfillment-service/types';
 import type {
   AppProxyContext as IAppProxyContext,
   AppProxyContextWithSession as IAppProxyContextWithSession,
 } from './authenticate/public/appProxy/types';
-import type {CheckoutContext as ICheckoutContext} from './authenticate/public/checkout/types';
-import type {CustomerAccountContext as ICustomerAccountContext} from './authenticate/public/customer-account/types';
 import type {
   AdminApiContext as IAdminApiContext,
   StorefrontContext as IStorefrontContext,
 } from './clients';
+
+// Direct re-exports for types without transformations
+export type {UnauthenticatedAdminContext} from './unauthenticated/admin/types';
+export type {UnauthenticatedStorefrontContext} from './unauthenticated/storefront/types';
+export type {FlowContext} from './authenticate/flow/types';
+export type {FulfillmentServiceContext} from './authenticate/fulfillment-service/types';
+export type {ScopesDetail} from './authenticate/admin/scope/types';
+export type {AdminApiContext} from './clients';
+export type {ScopesContext} from './authenticate/admin/types';
+export type {CheckoutContext} from './authenticate/public/checkout/types';
+export type {CustomerAccountContext} from './authenticate/public/customer-account/types';
+export type {StorefrontContext as StorefrontApiContext} from './clients';
 
 type ShopifyConfig<App> =
   App extends ShopifyApp<infer Config extends AppConfigArg> ? Config : never;
 
 type DefaultApp = ShopifyApp<AppConfigArg>;
 
-export type UnauthenticatedAdminContext = IUnauthenticatedAdminContext;
-
+// Types that need transformations or generic parameters
 export type AdminContext<App = DefaultApp> = IAdminContext<ShopifyConfig<App>>;
 
-export type ScopesContext<_App = DefaultApp> = IScopesContext;
-export type ScopesDetail = IScopesDetail;
-
-export type UnauthenticatedStorefrontContext<_App = DefaultApp> =
-  IUnauthenticatedStorefrontContext;
-
-export type FlowContext = IFlowContext;
-
-export type FulfillmentServiceContext = IFulfillmentServiceContext;
-
+// Types that need modifications
 export type AppProxyContext = IAppProxyContext | IAppProxyContextWithSession;
-
-export type CheckoutContext<_App = DefaultApp> = ICheckoutContext;
-
-export type CustomerAccountContext<_App = DefaultApp> = ICustomerAccountContext;
 
 export type WebhookContext = IWebhookContext<string>;
 
-// Extra types for the Admin API context
-export type AdminApiContext<_App = DefaultApp> = IAdminApiContext;
+// Extra types for API contexts
+export type AdminGraphqlClient = IAdminApiContext['graphql'];
 
-export type AdminGraphqlClient<App = DefaultApp> =
-  AdminApiContext<App>['graphql'];
-
-// Extra types for the Storefront API context
-export type StorefrontApiContext<_App = DefaultApp> = IStorefrontContext;
-
-export type StorefrontGraphqlClient<App = DefaultApp> =
-  StorefrontApiContext<App>['graphql'];
+export type StorefrontGraphqlClient = IStorefrontContext['graphql'];
